6c1ef58ca6540a2839273474da7e35f1beadc7a7
jayanth <jayanth@gmail.com>
2025-08-01 19:15:52 +0000

    latest with tecases

--- python-api-yt/testcases/test_search.py
+++ python-api-yt/testcases/test_search.py
@@ -6,16 +6,22 @@ import json
 import logging
 from utilities.custom_exceptions import *
 
-@pytest.mark.usefixtures("setup")
-class Test_search_album():
-    #test album with q,type,market,limit,offset,include_external
-    def test_search(self,setup):
-        token=setup
-        resp=helper.get_search(token=token,q="master",type="album",limit=2,offset=1,include_external="audio")
-        print(resp)
-        print(resp.status_code)
-        assert resp.status_code==200, "failure due to not sucess 200 statuscode"
-        print(resp)
+
+
+
+
+
+
+
+@pytest.fixture(scope="class")
+def album_search_data(setup):
+    token=setup
+    resp=helper.get_search(token=token,q="master",type="album",limit=2,offset=1,include_external="audio")
+    # print(resp)
+    print(resp.status_code)
+    assert resp.status_code==200, "failure due to not sucess 200 statuscode"
+    # print(resp.json())
+        
       
         #validate json schema
         try:
@@ -23,7 +29,33 @@ class Test_search_album():
             logging.info("this is success")
         except Exception as e:
             raise JsonschemaException("json schema failed")
-        #this is the first testcase
+    return {"data":resp.json()}
+
+
+
+@pytest.mark.usefixtures("album_search_data")
+class Test_search_album():
+   
+  
+  
+
+
+
+  
+    #TC01
+   
+    # def test_album_check(self,album_search_data):
+    #     self.data=album_search_data["data"]
+    def test_album(self,album_search_data):
+        # print(album_search_data)
+        assert album_search_data["data"]["albums"]["limit"]==2 ,"this is not expected"
+        assert album_search_data["data"]["albums"]["offset"]==1, "offset failure"
+        for i in album_search_data["data"]["albums"]["items"]:
+            assert i["album_type"]=="album",f"expected album but got {i['album_type']}"
+            
+        
+
+
     
 
             
